b86a7a286adc972b70af98981bde3cf4ca818fd0
Sergey Nasypaev <szergiusz777@gmail.com>
2016-09-19 14:57:56 +0300

    Service Tour Of Hero end. Run Routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
@@ -64,19 +65,28 @@ import { HeroService } from './hero.service';
         margin-right: .8em;
         border-radius: 4px 0 0 4px;
     }
-    `]
+    `],
+   providers: [HeroService]
 })
 
-heroService = new HeroService();
-heroes: Hero[];
-
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-  heroes = HEROES;
-  
+  heroes: Hero[]; 
   selectedHero: Hero;
 
   onSelect(hero: Hero): void{
       this.selectedHero = hero;
+  }
+
+  constructor(private heroService: HeroService) {
+
+  }
+
+  getHeroes(): void {
+      this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+      this.getHeroes();
   }
 }
